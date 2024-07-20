9bb6f77ace3df3e23cba7d56a8b7480563437621
Jimmy Rasmussen <jwr@lunar.app>
2024-07-20 10:16:14 +0200

    Copy from 11ty/eleventy-base-blog

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Developer Blog",
+	url: "https://example.com/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Jimmy Weibel Rasmussen",
+		email: "jweibel_22@hotmail.com",
+		url: "https://example.com/about-me/"
+	}
+}
